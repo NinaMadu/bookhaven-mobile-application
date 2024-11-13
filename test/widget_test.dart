29d3b3ae4a00fd9fbc3661ae2289f0de90697735
Imalsha-Ranasinghe <imalsharanasinghe2020@gmail.com>
2024-11-13 14:06:09 +0530

    Signin/up UI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bookshop/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
