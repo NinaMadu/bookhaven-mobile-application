e7ab9b19e315d28bc6c4f28243bf84d647ba7c2f
NinaMadu <ninadamaduwantha@gmail.com>
2024-12-02 22:02:46 +0530

    appbar separated

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:bookshop/pages/appbar.dart';
 import 'package:bookshop/pages/cartpage.dart';
 import 'package:bookshop/pages/favourites.dart';
 import 'package:bookshop/pages/notifications.dart';
@@ -274,134 +275,134 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  AppBar appBar(BuildContext context) {
-    final userId =
-        getCurrentUserId(); // Replace with your method to get the current user's ID.
+  // AppBar appBar(BuildContext context) {
+  //   final userId =
+  //       getCurrentUserId(); // Replace with your method to get the current user's ID.
 
-    return AppBar(
-      title: const Text(
-        'Book Haven',
-        style: TextStyle(color: Colors.black),
-      ),
-      backgroundColor: Colors.white,
-      elevation: 0,
-      actions: [
-        // Notification Icon
-        StreamBuilder<DocumentSnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection(
-                  'users') // Replace with your Firestore collection name
-              .doc(userId)
-              .snapshots(),
-          builder: (context, snapshot) {
-            final cartCount = (snapshot.data?.data()
-                        as Map<String, dynamic>?)?['notifications']
-                    ?.length ??
-                0;
+  //   return AppBar(
+  //     title: const Text(
+  //       'Book Haven',
+  //       style: TextStyle(color: Colors.black),
+  //     ),
+  //     backgroundColor: Colors.white,
+  //     elevation: 0,
+  //     actions: [
+  //       // Notification Icon
+  //       StreamBuilder<DocumentSnapshot>(
+  //         stream: FirebaseFirestore.instance
+  //             .collection(
+  //                 'users') // Replace with your Firestore collection name
+  //             .doc(userId)
+  //             .snapshots(),
+  //         builder: (context, snapshot) {
+  //           final cartCount = (snapshot.data?.data()
+  //                       as Map<String, dynamic>?)?['notifications']
+  //                   ?.length ??
+  //               0;
 
-            return IconButton(
-              icon: Stack(
-                children: [
-                  const Icon(Icons.notifications,
-                      color: Colors.black), // Cart icon
-                  if (cartCount > 0) // Show count if cart is not empty
-                    Positioned(
-                      right: 0,
-                      top: 0,
-                      child: Container(
-                        padding: const EdgeInsets.all(2),
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          shape: BoxShape.circle,
-                        ),
-                        constraints: const BoxConstraints(
-                          minWidth: 16,
-                          minHeight: 16,
-                        ),
-                        child: Text(
-                          '$cartCount',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 10,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                ],
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        NotificationsPage(), // Navigate to CartPage
-                  ),
-                );
-              },
-            );
-          },
-        ),
+  //           return IconButton(
+  //             icon: Stack(
+  //               children: [
+  //                 const Icon(Icons.notifications,
+  //                     color: Colors.black), // Cart icon
+  //                 if (cartCount > 0) // Show count if cart is not empty
+  //                   Positioned(
+  //                     right: 0,
+  //                     top: 0,
+  //                     child: Container(
+  //                       padding: const EdgeInsets.all(2),
+  //                       decoration: BoxDecoration(
+  //                         color: Colors.red,
+  //                         shape: BoxShape.circle,
+  //                       ),
+  //                       constraints: const BoxConstraints(
+  //                         minWidth: 16,
+  //                         minHeight: 16,
+  //                       ),
+  //                       child: Text(
+  //                         '$cartCount',
+  //                         style: const TextStyle(
+  //                           color: Colors.white,
+  //                           fontSize: 10,
+  //                           fontWeight: FontWeight.bold,
+  //                         ),
+  //                         textAlign: TextAlign.center,
+  //                       ),
+  //                     ),
+  //                   ),
+  //               ],
+  //             ),
+  //             onPressed: () {
+  //               Navigator.push(
+  //                 context,
+  //                 MaterialPageRoute(
+  //                   builder: (context) =>
+  //                       NotificationsPage(), // Navigate to CartPage
+  //                 ),
+  //               );
+  //             },
+  //           );
+  //         },
+  //       ),
 
-        StreamBuilder<DocumentSnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection(
-                  'users') // Replace with your Firestore collection name
-              .doc(userId)
-              .snapshots(),
-          builder: (context, snapshot) {
-            final cartCount =
-                (snapshot.data?.data() as Map<String, dynamic>?)?['cart']
-                        ?.length ??
-                    0;
+  //       StreamBuilder<DocumentSnapshot>(
+  //         stream: FirebaseFirestore.instance
+  //             .collection(
+  //                 'users') // Replace with your Firestore collection name
+  //             .doc(userId)
+  //             .snapshots(),
+  //         builder: (context, snapshot) {
+  //           final cartCount =
+  //               (snapshot.data?.data() as Map<String, dynamic>?)?['cart']
+  //                       ?.length ??
+  //                   0;
 
-            return IconButton(
-              icon: Stack(
-                children: [
-                  const Icon(Icons.shopping_cart,
-                      color: Colors.black), // Cart icon
-                  if (cartCount > 0) // Show count if cart is not empty
-                    Positioned(
-                      right: 0,
-                      top: 0,
-                      child: Container(
-                        padding: const EdgeInsets.all(2),
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          shape: BoxShape.circle,
-                        ),
-                        constraints: const BoxConstraints(
-                          minWidth: 16,
-                          minHeight: 16,
-                        ),
-                        child: Text(
-                          '$cartCount',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 10,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                ],
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CartPage(), // Navigate to CartPage
-                  ),
-                );
-              },
-            );
-          },
-        ),
-      ],
-    );
-  }
+  //           return IconButton(
+  //             icon: Stack(
+  //               children: [
+  //                 const Icon(Icons.shopping_cart,
+  //                     color: Colors.black), // Cart icon
+  //                 if (cartCount > 0) // Show count if cart is not empty
+  //                   Positioned(
+  //                     right: 0,
+  //                     top: 0,
+  //                     child: Container(
+  //                       padding: const EdgeInsets.all(2),
+  //                       decoration: BoxDecoration(
+  //                         color: Colors.red,
+  //                         shape: BoxShape.circle,
+  //                       ),
+  //                       constraints: const BoxConstraints(
+  //                         minWidth: 16,
+  //                         minHeight: 16,
+  //                       ),
+  //                       child: Text(
+  //                         '$cartCount',
+  //                         style: const TextStyle(
+  //                           color: Colors.white,
+  //                           fontSize: 10,
+  //                           fontWeight: FontWeight.bold,
+  //                         ),
+  //                         textAlign: TextAlign.center,
+  //                       ),
+  //                     ),
+  //                   ),
+  //               ],
+  //             ),
+  //             onPressed: () {
+  //               Navigator.push(
+  //                 context,
+  //                 MaterialPageRoute(
+  //                   builder: (context) => CartPage(), // Navigate to CartPage
+  //                 ),
+  //               );
+  //             },
+  //           );
+  //         },
+  //       ),
+  //     ],
+  //   );
+  // }
 
   Widget bookCard(String title, String image, String price, String author,
       String description, String bookId) {
@@ -550,7 +551,7 @@ class _HomePageState extends State<HomePage> {
 
       print('Book added to favourites');
     } catch (e) {
-      print('Error adding to favourites: $e');
+      print('Error adding to favorites: $e');
     }
   }
 }
